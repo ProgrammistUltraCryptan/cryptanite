d73a6600778f37535ade70f5fa03de734071e52f
ProgrammistUltraCryptan <137516713+ProgrammistUltraCryptan@users.noreply.github.com>
2023-06-23 16:50:23 +0300

    Create calculator.py
    Functionality

---
+++ calculator.py
@@ -0,0 +1,35 @@
+def calculator():
+    print("Simple Calculator")
+    print("Enter 'q' to quit")
+
+    while True:
+        try:
+            num1 = float(input("Enter the first number: "))
+            operator = input("Enter an operator (+, -, *, /): ")
+            num2 = float(input("Enter the second number: "))
+
+            if operator == '+':
+                result = num1 + num2
+            elif operator == '-':
+                result = num1 - num2
+            elif operator == '*':
+                result = num1 * num2
+            elif operator == '/':
+                if num2 == 0:
+                    print("Error: Division by zero is not allowed")
+                    continue
+                result = num1 / num2
+            else:
+                print("Error: Invalid operator")
+                continue
+
+            print("Result:", result)
+
+        except ValueError:
+            print("Error: Invalid input")
+        except KeyboardInterrupt:
+            print("\nCalculator stopped")
+            break
+
+
+calculator()
